330313689acb0c8c145e280a22345d4b4870fa25
ccimolai <153072332+ccimolai@users.noreply.github.com>
2024-02-26 13:02:33 +0100

    Update 4-HWR_vars_raster.R

--- 4-HWR_vars_raster.R
+++ 4-HWR_vars_raster.R
@@ -13,41 +13,58 @@ writeRaster(tg, filename = file.path("./5_rastervar", "tmax_int_max.tif"), overw
 #TRENDS
 #Function: calculate trends and significance 
   
-audrey_2<-function(tg,open=FALSE){
-    ### takes a raster, computes anual means and returns a stack with trend (OLS) and pvalue  
-    ## tg is a raster with annual values
-    if(open){tg<-brick(tg)}  
-    time<-as.numeric(substring(names(tg),2,5))
-    lm_fun = function(x) { if (is.na(x[1])){ NA } else { m = lm(x ~ time); summary(m)$coefficients[2] }} # slope
-    regressed<-calc(tg,lm_fun)*10 # times 10 = per decade
-    lm_fun2 = function(x) { if (is.na(x[1])){ NA } else { m = lm(x ~ time); summary(m)$coefficients[8] }} # pval
-    pvalues<-calc(tg,lm_fun2)
-    trends<-stack(regressed,pvalues)
-    return(trends)
+audrey_2 <- function(tg, open = FALSE) {
+  ### Toma un raster, calcula medias anuales y devuelve un stack con trend (OLS) y valor p
+  ## tg es un raster con valores anuales
+  if (open) {
+    tg <- brick(tg)
   }
+  time <- as.numeric(substring(names(tg), 2, 5))
+  
+  # Definir función para aplicar a cada píxel
+  lm_fun <- function(x) {
+    if (all(is.na(x))) {
+      trend <- NA  # Si todas las capas son NA, devolver NA
+    } else {
+      x <- ifelse(is.na(x), 0, x)  # Considerar NA como cero
+      m <- zyp.zhang(x, time)
+      trend <- m["trend"]* 10 #per decade
+      
+    }
+    return(trend)
+  } # slope
+  
+  # Aplicar la función a cada píxel
+  regressed <- calc(tg, fun = lm_fun) 
+  
+  # Definir función para calcular p-values
+  lm_fun2 <- function(x) {
+    if (all(is.na(x))) {
+      p_value <- NA  # Si todas las capas son NA, devolver NA
+    } else {
+      x <- ifelse(is.na(x), 0, x)  # Considerar NA como cero
+      m <- zyp.zhang(x, time)
+      p_value <- m["sig"]
+    }
+    return(p_value)
+  } # pval
+  
+  # Aplicar la función a cada píxel
+  pvalues <- calc(tg, fun = lm_fun2)
+  
+  # Apilar resultados
+  trends <- stack(regressed, pvalues)
+  
+  return(trends)
+}
+
 trends<- audrey_2(tg)
 
-#******************************************************************************************
-# argentina´s limits if needed
-  area <- readOGR("./limites_Arg/Argentina_limites.shp") # abrimos el fichero para la mascara
-  crs(area) <- "EPSG:4326" # definimos el sistema de coordenadas
-  a_crs_object_epsg <-CRS("+proj=longlat +datum=WGS84 +no_defs +init=epsg:4326")
-  area <- spTransform(area, a_crs_object_epsg)
-  
-#******************************************************************************************  
-##CUT RASTER (IF NEEDED)
-projection(tg) <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
-projection(trends) <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
-#var_duration = raster::crop(tg, area) %>% raster::mask(area) 
-#var_trend = raster::crop(trends, area) %>% raster::mask(area) 
-m1<- raster::mean(var_duration, na.rm= TRUE)
-  
-writeRaster(m1, filename = file.path("./6_stats", "tmax_duracion_mean.tif"), overwrite=TRUE)
+#**************************************************************************************************************
+#* Create a raster displaying the values of significant trends.
 
-#******************************************************************************************  
-# Create a new layer only with significant trends(p_value<=0.05)
-ttt<- var_trend[[1]]
-ppp<- var_trend[[2]]
-val_sig <- overlay(ttt, ppp, fun = function(x,y) {ifelse(y <= 0.05, x, NA)})
-writeRaster(val_sig, filename = file.path("./6_stats", "tmax_duracion_trend.tif"), overwrite=TRUE)
+val_sig_Z<- overlay(trends$layer.1, trends$layer.2, fun = function(x,y) {ifelse(y <= 0.05, x, NA)})
+writeRaster(val_sig_Z, filename = ".....", overwrite=TRUE)
+
+
   
